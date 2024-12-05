5401451a4e165ea0604ac3ec89bcadcfe6decbcd
kennethfan <kennethfan137@gmail.com>
2024-12-05 18:01:40 +0800

    echo

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 import sys
 
 
+def echo_command(command: str):
+    if command == 'echo':
+        print('')
+        return True
+    if command.startswith('echo '):
+        print(command[5:])
+        return True
+    return False
+
+
 def main():
     exit_command = 'exit 0'
     valid_commands = [exit_command]
@@ -11,11 +21,11 @@ def main():
 
         # Wait for user input
         command = input()
-        if command not in valid_commands:
-            print(f'{command}: command not found')
-            continue
         if exit_command == command:
             sys.exit(0)
+        if echo_command(command):
+            continue
+        print(f'{command}: command not found')
 
 
 if __name__ == "__main__":
