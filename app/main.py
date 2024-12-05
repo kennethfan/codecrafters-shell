ca5cbb2e94e409201e0e1872dac92eaf84276b22
kennethfan <kennethfan137@gmail.com>
2024-12-05 17:56:02 +0800

    main loop

--- app/main.py
+++ app/main.py
@@ -2,12 +2,17 @@ import sys
 
 
 def main():
-    # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
+    valid_commands = []
+    while True:
+        # Uncomment this block to pass the first stage
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
 
-    # Wait for user input
-    command = input()
-    print(f'{command}: not found')
+        # Wait for user input
+        command = input()
+        if command not in valid_commands:
+            print(f'{command}: command not found')
+            continue
 
 
 if __name__ == "__main__":
