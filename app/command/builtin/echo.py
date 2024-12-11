1f978b6f0481891af03e961c1de0ac50076979a0
kennethfan <kennethfan137@gmail.com>
2024-12-11 14:03:33 +0800

    fix echo

--- app/command/builtin/echo.py
+++ app/command/builtin/echo.py
@@ -6,6 +6,4 @@ class Echo(Command):
         if len(self.args) == 1:
             print('')
             return
-        for arg in self.args[1:]:
-            print(arg, end=' ')
-        print('')
+        print(' '.join(self.args[1:]))
