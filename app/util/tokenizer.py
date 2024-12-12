42e3605db1436ad1c9d30d7c7ff07c256da58ab0
kennethfan <kennethfan137@gmail.com>
2024-12-12 12:26:37 +0800

    double quote

--- app/util/tokenizer.py
+++ app/util/tokenizer.py
@@ -7,22 +7,26 @@ class Tokenizer:
     _SINGLE_QUOTE = '\''
     _DOUBLE_QUOTE = '"'
     _ESCAPE = '\\'
+    _DOLLAR = '$'
     _WHITE_SPACE = ' '
+    _TAB = '\t'
+    _CR = '\r'
+    _LF = '\n'
     _QUOTE_ESCAPE_MAPPING = {
-        'a': '\a',
-        'b': '\b',
-        't': '\t',
-        'r': '\r',
-        'n': '\n',
+        't': _TAB,
+        'r': _CR,
+        'n': _LF,
         _ESCAPE: _ESCAPE,
         _SINGLE_QUOTE: _SINGLE_QUOTE,
         _DOUBLE_QUOTE: _DOUBLE_QUOTE,
+        _DOLLAR: _DOLLAR,
     }
 
     def __init__(self, input_str: str):
         self._input_str = input_str
         self._length = len(input_str)
         self._in_single_quote = False
+        self._in_double_quote = False
         self._pos = 0
         self._token_list = []
         self._cur_token_chs = []
@@ -42,6 +46,8 @@ class Tokenizer:
     def _dispatch(self, ch):
         if ch == self._SINGLE_QUOTE:
             self._handle_single_quote()
+        elif ch == self._DOUBLE_QUOTE:
+            self._handle_double_quote()
         elif ch == self._ESCAPE:
             self._handle_escape()
         else:
@@ -69,29 +75,44 @@ class Tokenizer:
         self._cur_token_chs = []
 
     def _handle_single_quote(self):
-        # if self._in_single_quote:
-        #     self._token_end()
+        if self._in_double_quote:
+            self._cur_token_chs.append(self._SINGLE_QUOTE)
+            return
         self._in_single_quote = not self._in_single_quote
 
+    def _handle_double_quote(self):
+        if self._in_single_quote:
+            self._cur_token_chs.append(self._DOUBLE_QUOTE)
+            return
+        self._in_double_quote = not self._in_double_quote
+
     def _handle_escape(self):
+        if self._in_single_quote:
+            self._cur_token_chs.append(self._ESCAPE)
+            return
+
         ch = self._next_ch()
         if ch is None:
             self._error()
             return
-        if self._in_single_quote:
+
+        if self._in_double_quote:
             mp_ch = self._QUOTE_ESCAPE_MAPPING.get(ch)
             if mp_ch is None:
-                self._error()
+                self._cur_token_chs.append(ch)
                 return
             self._cur_token_chs.append(mp_ch)
             return
-        if self._WHITE_SPACE != ch:
-            self._error()
+
+        if self._WHITE_SPACE == ch:
+            self._cur_token_chs.append(self._WHITE_SPACE)
             return
-        self._cur_token_chs.append(self._WHITE_SPACE)
+        if self._LF == ch:
+            return
+        self._cur_token_chs.append(ch)
 
     def _handle_default(self, ch: str):
-        if ch.isspace() and not self._in_single_quote:
+        if ch.isspace() and not self._in_single_quote and not self._in_double_quote:
             self._token_end()
             return
         self._cur_token_chs.append(ch)
@@ -99,6 +120,6 @@ class Tokenizer:
 
 if '__main__' == __name__:
     test_str = """
-    'adsajfld ' dajkfjdsa' 'fdsajl ' fdasfdsal fdsalf kdsa'
+    "adsajfld '" "dajkfjdsa'" '"fdsajl "' "fdasfdsal fdsalf kdsa"
     """
     print(Tokenizer(test_str).tokenize())
